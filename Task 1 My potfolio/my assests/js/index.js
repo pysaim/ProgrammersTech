d8c2e0f6489687b367df5a3d6aa3a1a60e8752d6
Saim Manan <147409447+pysaim@users.noreply.github.com>
2024-07-31 11:51:04 +0500

    uploded all files

---
+++ Task 1 My potfolio/my assests/js/index.js
@@ -0,0 +1,50 @@
+/*SERVIES MODAL */
+const modal = document.querySelectorAll('.services__modal'),
+    modalButton = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
+
+let activeModal = (modalClick) => {
+    modal[modalClick].classList.add('active-modal')
+}
+modalButton.forEach((modalButton, i) => {
+    modalButton.addEventListener('click', () => {
+        activeModal(i)
+    })
+
+})
+
+modalClose.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modal.forEach((modalRemove) => {
+            modalRemove.classList.remove('active-modal')
+        })
+
+    })
+})
+
+/* SWIPER TESTIMONIAL */
+const swiperTestionial = new Swiper('.testimonial__swiper', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+        clickable: true,
+    },
+
+})
+
+
+
+/* SHOW SCROLL UP */
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-up id
+    window.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                          : scrollUp.classList.remove('show-scroll');
+};
+
+window.addEventListener('scroll', scrollUp);
+
